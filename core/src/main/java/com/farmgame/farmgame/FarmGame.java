d8109441f9cd4fe1e6ec5000ac978e4960cfeec2
Joe-Newm <j.newm500@gmail.com>
2025-04-13 16:03:10 -0500

    add viewport and camera, window resizes correctly

--- core/src/main/java/com/farmgame/farmgame/FarmGame.java
+++ core/src/main/java/com/farmgame/farmgame/FarmGame.java
@@ -2,10 +2,11 @@ package com.farmgame.farmgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.farmgame.farmgame.Player;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -13,21 +14,35 @@ public class FarmGame extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
     private Player player;
+    public Viewport viewport;
+    public Camera camera;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
         player = new Player(5);
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(1280, 720,camera);
+        Gdx.graphics.setWindowedMode(1280, 720);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0f, 0f, 0f, 1f);
+        camera.update();
+        viewport.apply();
+        batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
         batch.draw(image, 140, 210);
         player.draw(batch);
         batch.end();
+    }
+
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
     }
 
     @Override
